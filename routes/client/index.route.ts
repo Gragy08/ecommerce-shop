cc94569845a0009a941b2a6530695f6b346e537e
Gragy08 <giaqui1712@gmail.com>
2025-12-21 13:12:24 +0700

    Hiển thị dữ liệu ra Mini Cart

--- routes/client/index.route.ts
+++ routes/client/index.route.ts
@@ -2,6 +2,7 @@ import { Router } from "express";
 import homeRoutes from "./home.route";
 import articleRoutes from "./article.route";
 import productRoutes from "./product.route";
+import cartRoutes from "./cart.route";
 import * as categoryMiddleware from "../../middlewares/client/category.middleware";
 import * as attributeMiddleware from "../../middlewares/client/attribute.middleware";
 
@@ -16,5 +17,7 @@ router.use('/', homeRoutes);
 router.use('/article', articleRoutes);
 
 router.use('/product', productRoutes);
+
+router.use('/cart', cartRoutes);
 
 export default router;
