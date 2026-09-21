cc85cb44bf2c6af96d216d10004ccca38fc99605
Gragy08 <giaqui1712@gmail.com>
2026-09-21 11:48:11 +0700

    Trang Danh sách địa chỉ

--- controllers/client/dashboard.controller.ts
+++ controllers/client/dashboard.controller.ts
@@ -94,9 +94,20 @@ export const changePassword = (req: Request, res: Response) => {
   });
 }
 
-export const address = (req: Request, res: Response) => {
+export const address = async (req: Request, res: Response) => {
+  const id = res.locals.accountUser.id;
+
+  const addressList = await UserAddress
+    .find({
+      userId: id
+    })
+    .sort({
+      createdAt: "desc"
+    })
+
   res.render("client/pages/dashboard-address", {
-    pageTitle: "Danh sách địa chỉ"
+    pageTitle: "Danh sách địa chỉ",
+    addressList: addressList
   });
 }
 
