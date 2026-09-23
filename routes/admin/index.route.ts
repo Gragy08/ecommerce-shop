e27dd4e74f717a59f4b60953c0e010e56358ffd4
Gragy08 <giaqui1712@gmail.com>
2026-09-23 11:15:57 +0700

    Quản lý tài khoản người dùng bên admin

--- routes/admin/index.route.ts
+++ routes/admin/index.route.ts
@@ -9,6 +9,7 @@ import accountRoutes from "./account.route";
 import adminLogRoutes from "./admin-log.route";
 import productRoutes from "./product.route";
 import couponRoutes from "./coupon.route";
+import accountUserRoutes from "./account-user.route";
 
 import * as authMiddleware from "../../middlewares/admin/authenticate.middleware";
 
@@ -24,6 +25,7 @@ router.use('/admin-log', authMiddleware.verifyToken, adminLogRoutes);
 router.use('/account', accountRoutes);
 router.use('/product', authMiddleware.verifyToken, productRoutes);
 router.use('/coupon', authMiddleware.verifyToken, couponRoutes);
+router.use('/account-user', authMiddleware.verifyToken, accountUserRoutes);
 
 
 export default router;
